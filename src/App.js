d262a2b833d3e319d440cd54fa7d728dd44147ee
urw7rs <urw.7rs@icloud.com>
2021-12-30 03:21:21 +0900

    make it react compatible / react에 맞게 수정

--- src/App.js
+++ src/App.js
@@ -1,41 +1,39 @@
-import './App.css';
+import "./App.css";
+
+const axios = require("axios");
 
 function App() {
+  function submitButton_click() {
+    let context = document.getElementById("#context");
+    let question = document.getElementById("#question");
+    let answer = document.getElementById("#answer");
+
+    axios
+      .post("/submit", {
+        context: context.value,
+        question: question.value,
+      })
+      .then(function (response) {
+        answer.value = response;
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+
   return (
     <div className="App">
       <header className="App-header">
+        <label for="context">CONTEXT PARAGRAPH</label>
+        <textarea id="context" name="contexts" rows="5" cols="33"></textarea>
+        <label for="question">QUESTION</label>
+        <textarea id="question" name="question" rows="3" cols="33"></textarea>
+        <label for="answer">ANSWER</label>
+        <textarea id="answer" name="answer" rows="3" cols="33"></textarea>
+        <button id="submitButton" onclick={submitButton_click}>
+          Submit
+        </button>
       </header>
-
-      <label for="contexts">CONTEXT PARAGRAPH</label>
-      <textarea id="contexts" name="contexts" rows="5" cols="33"></textarea>
-
-      <label for="question">QUESTION</label>
-      <textarea id="question" name="question" rows="3" cols="33"></textarea>
-    
-      <label for="answer">ANSWER</label>
-      <textarea id="answer" name="answer" rows="3" cols="33"></textarea>
-
-      <button id="submitButton" onclick="submitButton_click();">Submit</button>
-
-      <script>
-        /*왜 오류가 날까요;ㅅ;*/
-        function submitButton_click() {
-          let contextsValue = document.getElementById('#contexts');
-          let questionValue = document.getElementById('#question');
-          let answerValue = document.getElementById('#answer');
-
-          axios.post('/submit', {
-            context: contextsValue.value,
-            question: question.value,
-          })
-          .then(function (response) {
-            answer.value = response;
-          })
-          .catch(function (error) {
-            console.log(error);
-          })
-        }
-      </script>
     </div>
   );
 }

328bd939a4bc55eaa24b48844c07ddd8a27c84dc
urw7rs <urw.7rs@icloud.com>
2022-01-05 12:18:56 +0900

    save state in variable

--- extension/background.js
+++ extension/background.js
@@ -8,45 +8,34 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
 });
 
 chrome.runtime.onConnect.addListener((port) => {
-  if (port.name === "question")
-    port.onMessage.addListener((msg) => {
-      const question = msg.question;
+  port.postMessage({ prevQA });
 
-      const url = "http://192.168.35.183:5000/answer";
-      const options = {
-        method: "POST",
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          context: context,
-          prev_qa: prevQA,
-          question: question,
-        }),
-      };
+  port.onMessage.addListener((msg) => {
+    const question = msg.question;
 
-      fetch(url, options)
-        .then((response) => response.json())
-        .then((data) => {
-          let answer = data.answer;
-          // TODO: print answer to chat box
-          console.log(answer);
-          port.postMessage({ answer });
-          prevQA.push(question);
-          prevQA.push(answer);
-        })
-        .catch((error) => console.log(error));
-    });
-});
+    const url = "http://localhost:5000/answer";
+    const options = {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        context: context,
+        prev_qa: prevQA,
+        question: question,
+      }),
+    };
 
-/*
-chrome.action.onClicked.addListener((tab) => {
-  chrome.scripting.executeScript({
-    target: { tabId: tab.id },
-    function: () => {
-      chrome.runtime.sendMessage({ context: $("p").text() });
-    },
+    fetch(url, options)
+      .then((response) => response.json())
+      .then((data) => {
+        let answer = data.answer;
+        prevQA.push(question);
+        prevQA.push(answer);
+
+        port.postMessage({ answer });
+      })
+      .catch((error) => console.log(error));
   });
 });
-*/
--- extension/popup.js
+++ extension/popup.js
@@ -1,23 +1,63 @@
+function addChat(msg, className) {
+  const newDiv = document.createElement("div");
+  const newP = document.createElement("p");
+  const newContent = document.createTextNode(msg);
+  newDiv.className = className;
+  newP.appendChild(newContent);
+  newDiv.appendChild(newP);
+
+  const currentDiv = document.getElementsByClassName("input-container")[0];
+  document.body.insertBefore(newDiv, currentDiv);
+}
+
+function addQuestion(msg) {
+  addChat(msg, "container");
+}
+
+function addAnswer(msg) {
+  addChat(msg, "container darker");
+}
+
+const port = chrome.runtime.connect({ name: "question" });
+
+let prevQA;
+port.onMessage.addListener((msg) => {
+  if (msg.prevQA) {
+    prevQA = msg.prevQA || [];
+
+    let question = true;
+    for (let chat of prevQA) {
+      if (question) {
+        addQuestion(chat);
+      } else {
+        addAnswer(chat);
+      }
+      question = !question;
+    }
+  }
+
+  if (msg.answer) {
+    addAnswer(msg.answer);
+  }
+});
+
 document.addEventListener("DOMContentLoaded", () => {
+  // load previous quesitons and answers
+
   const button = document.getElementById("send");
   const input = document.getElementById("input");
-  const port = chrome.runtime.connect({ name: "question" });
 
+  // send question to background.js
   button.addEventListener("click", () => {
     const question = input.value;
-    port.postMessage({ question });
-
-    const newDiv = document.createElement("div");
-    const newP = document.createElement("p");
-    const newContent = document.createTextNode(question);
-    newDiv.className = "container";
-    newP.appendChild(newContent);
-    newDiv.appendChild(newP);
-
-    const currentDiv = document.getElementsByClassName("input-container")[0];
-    document.body.insertBefore(newDiv, currentDiv);
+    // if input is not empty
+    if (question) {
+      port.postMessage({ question });
+      addQuestion(question);
+    }
   });
 
+  // click button when enter is pressed
   input.addEventListener("keydown", (e) => {
     if (e.key === "Enter") {
       e.preventDefault();
